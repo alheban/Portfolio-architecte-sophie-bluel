553e3a0b213a66878e19b9eb92db5c9ee0f71822
annelise <alheban@gmail.com>
2023-12-06 13:24:58 +0100

    etape 3.1 ajout style delete modal

--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -1,6 +1,4 @@
 
-let listProjets = []; // Initialisation avec un tableau vide
-
 async function getWorksApi() {
   try {
     const response = await fetch("http://localhost:5678/api/works");
@@ -15,19 +13,22 @@ async function getWorksApi() {
   }
 }
 
+let listProjets = []; // Initialisation avec un tableau vide
+
 /*------ Sélectionner la section dans le DOM-------*/
 const sectionFiltre = document.getElementById("portfolio");
 const sectionPortfolio = document.querySelector(".gallery");
 const sectionModal = document.querySelector(".galerie_modal");
 
-/*------ creation div avec les boutons filtre projet-------*/
+
+/*------ boutons pour filtrer projet-----------------------*/
+//creation div avec les boutons filtre 
 const divFiltre = document.createElement("div");
 divFiltre.classList.add("filtre");
 sectionFiltre.appendChild(divFiltre);
-// Ajouter la nouvelle div comme premier enfant à l'élément avec l'ID "portfolio"
 sectionFiltre.insertBefore(divFiltre, sectionFiltre.children[1]);
 
-/* -----------------creation du bouton tous -----------------------*/
+//creation du bouton tous
 const tousButton = document.createElement("button");
 tousButton.innerText = "Tous";
 divFiltre.appendChild(tousButton);
@@ -37,21 +38,7 @@ tousButton.addEventListener("click", () => {
   activerBouton(tousButton);
 });
 
-
-
-/* -----------------Fonction pour creer les boutons par catégories-----------------------*/
-  function creerBoutonFiltre(categorieName) {
-    const buttonFiltre = document.createElement("button");
-    buttonFiltre.innerText = categorieName;
-    buttonFiltre.addEventListener("click", () => {
-      filtrerParCategorie(categorieName);
-      activerBouton(buttonFiltre);
-      
-    });
-    return buttonFiltre;
-  }
-
-/* -----------------Fonction pour filtrer et afficher les boutons par catégories sans doublons-----------------------*/
+//Fonction pour filtrer et afficher les boutons par catégories sans doublons
 function filtrerLesBouton() {
   // Créer un ensemble pour suivre les catégoriesname déjà ajoutées
   const categoriesNameAjoutees = new Set();
@@ -65,24 +52,35 @@ function filtrerLesBouton() {
   });
   
 }
-/* -----------------Fonction pour ajouter la classe "active" au bouton filtres cliqué -----------------------*/
+//Fonction pour creer les boutons par catégories
+  function creerBoutonFiltre(categorieName) {
+    const buttonFiltre = document.createElement("button");
+    buttonFiltre.innerText = categorieName;
+    buttonFiltre.addEventListener("click", () => {
+      filtrerParCategorie(categorieName);
+      activerBouton(buttonFiltre);     
+    });
+    return buttonFiltre;
+  }
+
+//Fonction activer la classe "active" au bouton
 function activerBouton(bouton) {
-  // Retirer la classe "active" de tous les boutons
   document.querySelectorAll(".filtre button").forEach((b) => b.classList.remove("active"));
   bouton.classList.add("active");
 }
-
-/* -----------------Filtrer les projets en fonction de la catégorie sélectionnée -----------------------*/
+/* -----------------galerie----------------------------------*/
+//Filtrer les projets en fonction de la catégorie sélectionnée
 function filtrerParCategorie(categorieName) {
   const projetsFiltres = listProjets.filter((work) => work.category.name === categorieName);
   creerGalerieProjets(projetsFiltres);
 }
 
-/* -----------------fonction creation galerie des projets-----------------------*/
+//fonction creation galerie des projets
 function creerGalerieProjets(projetsgalerie) {
   sectionPortfolio.innerHTML = ""; // Nettoyer la section de la galerie avant d'ajouter les nouveaux projets
-  // Boucle galerie pour chaque projet filtré
-  projetsgalerie.forEach((work) => {
+  sectionModal.innerHTML = "";
+
+  projetsgalerie.forEach((work) => { // Boucle galerie pour chaque projet filtré
     const figureElement = document.createElement("figure");
     const imageElement = document.createElement("img");
     imageElement.src = work.imageUrl;
@@ -96,12 +94,20 @@ function creerGalerieProjets(projetsgalerie) {
     //galerie modal
     const figureModalElement = document.createElement("figure");
     const imageModalElement = document.createElement("img");
+    imageModalElement.classList.add ("imageModal")
     imageModalElement.src = work.imageUrl;
     sectionModal.appendChild(figureModalElement);
     figureModalElement.appendChild(imageModalElement);
-
+ //btn delete
+ const btnPath = "./assets/icons/delete.svg"; 
+ const btnDeleteElement = document.createElement("img");
+ btnDeleteElement.classList.add ("btndelete");
+ btnDeleteElement.src = btnPath;
+ figureModalElement.appendChild(btnDeleteElement);
+ figureModalElement.insertBefore(btnDeleteElement, figureModalElement.firstChild);
   });
 }
+
 /* -----------------Mode editeur-----------------------*/
 function isAuthe() {
   let token = localStorage.getItem("token");
@@ -136,9 +142,9 @@ function ajoutLogout() {
   logoutLien.href = "";
   logoutLien.addEventListener("click",() => { 
     localStorage.removeItem("token");
-
   });
 }
+
 
 // Appeler la fonction getWorksApi pour récupérer les projets depuis l'API
 getWorksApi().then((projetsRecuperes) => {
